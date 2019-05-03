23380e5efcedad457c9e1c549ba87d31e5cfaa2e
SardinPS <47413496+SardinPS@users.noreply.github.com>
2019-05-03 22:09:02 +0200

    Last commit

--- js/script.js
+++ js/script.js
@@ -23,16 +23,14 @@ $formSearch.on('submit', function(event) {
 
         for (let i = 0; i < songBySearch.data.length; i++) {
             var cover = songBySearch.data[i].album.cover_medium;
+            var title_id = songBySearch.data[i].id;
             var title = songBySearch.data[i].title;
             var artist = songBySearch.data[i].artist.name;
             var album =  songBySearch.data[i].album.title;
             var preview = songBySearch.data[i].preview;
-
            
             $results.append(`
-
-            <div class="card-group">
-                <div class="card" style="width: 21rem;">
+                <div class="card" style="width: 18rem;">
                         <img class="card-image-top" src=" `+cover+`" >
                         <div class="card-body">       
                             <h5 class="card-title">`+artist+` / `+title+`</h5>
@@ -40,71 +38,15 @@ $formSearch.on('submit', function(event) {
                             <audio controls src="`+preview+`">
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted"><button><i class="fa fa-heart fa-2x"></i> Ajouter aux favoris</button></small>
+                            <small class="text-muted"><button value="`+title_id+`" id="fav" class="btn btn-danger"><i class="fa fa-heart fa-2x"></i> Ajouter aux favoris</button></small>
                         </div>
-                    </div>
-                
-
+                </div>
             `
             
             )
         }
+        
     
     })
+    
 })})
-
-
-
-    /*getSongBySearch(song).then(title => {
-        $results.html(
-            `<div class="card" style="width: 22rem; margin: 50px auto;">
-                <div class="card-body">
-                ${title.data.map(title => (
-                    `   <img src="${title.album.cover}">
-                        ${title.album.title}
-                     <h4 class="card-title">${title.artist.name} / ${title.title}</h4>
-                     <button id="fav" style="margin: 10px auto;" value="${title.id}"><i class="fa fa-heart"></i>Ajouter au favoris</button>
-                    <audio controls src ="${title.preview}">
-                    `
-                )).join('')}
-                   
-                </div>
-            </div>`
-        );
-        var $fav = $('#fav').val();
-
-        $('#fav').click(function(){
-
-             localStorage.getItem($fav, $fav);
-
-
-            if(localStorage.getItem($fav, $fav) == true){
-                localStorage.removeItem($fav, $fav);
-            }
-            else{
-                localStorage.setItem($fav, $fav);
-            }
-
-            
-
-
-        })
-        
-        
-    });
-});
-
-
-
- 
-
-for (var i = 0; i < localStorage.length; i++) {
-
-    var key = localStorage.key(i);
-  
-    var value = localStorage.getItem(key);
-  
-    console.log('Key: ' + key + ', Value: ' + value);  
-}
-
-})*/
---
+++ readME.md
@@ -0,0 +1,12 @@
+Cette aplication requiert npm et live-server pour fonctionner correctement.
+
+Pour l'installer : Veuillez vous rendre sur l'url suivante https://www.npmjs.com/get-npm
+
+Une fois l'application configurée, pour installer la dernière version de npm, veuillez lancer votre invite de commande (ex. powershell) et collez la ligne suivante : npm install npm@latest -g
+
+Pour installer live-server, veuillez rentrer la ligne de commande suivante  dans votre invite de commande : npm i live-server
+
+Une fois effectué, pour lancer l'application, vérifiez que vous êtes bien à la racine des fichiers et collez la ligne suivante dans votre invite de commande : npm run dev
+
+
+
